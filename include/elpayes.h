d9ea328cb0cf3847779339a299543ffb574fec43
one.neuron <one.neuron@830a2072-bcc1-11dd-ad1f-f1f9622dbc98>
2008-12-02 16:07:53 +0000

    one.neuron

--- include/elpayes.h
+++ include/elpayes.h
@@ -3,6 +3,19 @@ char lock;
 int proces_monitoritzat;
 int sys_call_monitoritzat;
 
+struct dev_t * maj_min;
+struct cdev * new_dev;
+
+struct file_operations file_ops = {
+ owner: THIS_MODULE;
+ read: sys_read_dev;
+ open: sys_open_dev;
+ release: sys_release_dev;
+ ioctl: sys_ioctl_dev;
+};
+
 extern sysc_info sysc_info_table[];
 extern struct t_info;
 extern pids_monitoritzats[];
+
+extern N_CRIDES_A_MONITORITZAR
