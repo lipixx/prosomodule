585a357d186fad9df136de57f9a62d1eb9f04642
one.neuron <one.neuron@830a2072-bcc1-11dd-ad1f-f1f9622dbc98>
2008-12-02 16:07:33 +0000

    one.neuron

--- elpayes.c
+++ elpayes.c
@@ -14,17 +14,72 @@ MODULE_LICENSE("GPL");
 static int __init ir_al_huerto_init(void)
 {
 
-  alloc_chrdev_region(maj_min, 0, 1,"/dev/payes");
-  register_chrdev_region(maj_min, 1, "/dev/payes");
+  alloc_chrdev_region(maj_min, 0, 1,"payes");
+  register_chrdev_region(maj_min, 1, "payes");
   new_dev = cdev_alloc();
   new_dev->owner = THIS_MODULE;
   new_dev->ops=&file_ops;
   c_dev_add(new_dev,maj_min,1);
   lock=0;
+ 
+  printk(KERN_DEBUG "El pages arriba ben descansat a l'hort, preparat per una dura jornada de feina\n");
+
   return 0;
 }
 
 static void __exit salir_del_huerto(void)
 {
+  unregister_chrdev(maj_min,1);
+  cdev_del(dew_dev);
 
+  printk(KERN_DEBUG "El pages ha arribat a casa sa i estalvi, ningu l'hi ha robat els melons\n");
+
+}
+
+module_init(ir_al_huerto_init);
+module_exit(salir_del_huerto_exit);
+
+
+sys_open_dev(){
+
+}
+
+sys_read_dev(){
+}
+
+sys_release_dev(){
+
+}
+
+sys_ioctl_dev(){
+
+}
+
+reset_valors (proces_monitoritzat){
+
+}
+
+reset_tots_valors()
+{
+
+}
+
+activar_sys_call(int quina){
+  int i;
+  if(quina <-1 || quina > N_CRIDES_A_MONITORITZAR) return -EINVAL;
+  else if(quina!=-1) activar_monitoritzatcio(quina);
+  else
+    for(i=0; i < N_CRIDES_A_MONITORITZAR; i++)
+      activar_monitoritzacio(i);
+  return 0;
+}
+
+desactivar_sys_call(int quina){
+  int i;
+  if(quina <-1 || quina > N_CRIDES_A_MONITORITZAR) return -EINVAL;
+  else if(quina!=-1) desactivar_monitoritzacio(quina);
+  else
+    for(i=0; i < N_CRIDES_A_MONITORITZAR; i++)
+      desactivar_monitoritzacio(i);
+  return 0;
 }
