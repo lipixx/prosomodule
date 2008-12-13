287234b3e644131babe1a03d00817a2c4c02fc3e
one.neuron <one.neuron@830a2072-bcc1-11dd-ad1f-f1f9622dbc98>
2008-12-13 17:41:01 +0000

    one.neuron

--- elpayes.c
+++ elpayes.c
@@ -16,7 +16,7 @@ MODULE_LICENSE ("GPL");
 
 /* Inicialitzacio del modul. */
 
-static int __init
+static int__init
 ir_al_huerto_init (void)
 {
   int result;
@@ -37,7 +37,7 @@ ir_al_huerto_init (void)
   return 0;
 }
 
-static void __exit
+static void__exit
 salir_del_huerto_exit (void)
 {
   unregister_chrdev_region (*maj_min, 1);
