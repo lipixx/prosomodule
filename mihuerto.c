402d6ee71fe68f6ebe05429763321922f7c31e02
one.neuron <one.neuron@830a2072-bcc1-11dd-ad1f-f1f9622dbc98>
2008-12-13 11:53:16 +0000

    one.neuron

--- mihuerto.c
+++ mihuerto.c
@@ -494,11 +494,11 @@ obtenir_estadistiques (int pid, int crida, struct pid_stats *stats)
     return -EINVAL;
   if (pid < 0)
     return -EINVAL;
-  task =  (struct th_info_est *) (find_task_by_pid ((pid_t) pid))->thread_info;
+  task =  (struct th_info_est *) (find_task_by_pid ((pid_t) pid));
   if (task < 0)
     return -ESRCH;
 
-  task_stats = task->estadistiques[crida])
+  task_stats = task->estadistiques[crida];
 
   stats->num_entrades = task_stats->num_entrades;
   stats->num_sortides_ok = task_stats->num_sortides_ok;
