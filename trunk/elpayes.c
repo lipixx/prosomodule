48f61c5abb1f4b77db5c09c28790e1da4b527eb4
one.neuron <one.neuron@830a2072-bcc1-11dd-ad1f-f1f9622dbc98>
2008-12-14 18:32:23 +0000

    one.neuron

--- trunk/elpayes.c
+++ trunk/elpayes.c
@@ -104,7 +104,7 @@ pages_read_dev (struct file *f, char __user * buffer, size_t s, loff_t * off)
   else mida = sizeof (struct pid_stats);
 
   module_put(THIS_MODULE);
-  printk("\nhola\n%i%i%i%lld",info.num_entrades,info.num_sortides_ok,info.num_sortides_error,info.durada_total);
+  printk(KERN_DEBUG"\nhola\n%i%i%i%lld",info.num_entrades,info.num_sortides_ok,info.num_sortides_error,info.durada_total);
   return (ssize_t) copy_to_user (buffer,&info,mida);
   //  return (ssize_t) size;
 }
