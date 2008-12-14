91ab332a851b974bca479f32d877789a1e87b71c
lipixx <lipixx@830a2072-bcc1-11dd-ad1f-f1f9622dbc98>
2008-12-14 19:47:27 +0000

    Documentacio i jocs de proves 2

--- include/jp_elpayes.h
+++ include/jp_elpayes.h
@@ -1,3 +1,10 @@
+#define CH_PID 0
+#define CH_SYSC 1
+#define RESET_VALS 2
+#define RESET_ALL_VALS 3
+#define ACT_SYSC 4
+#define DEACT_SYSC 5
+
 struct pid_stats{
   int num_entrades;
   int num_sortides_ok;
