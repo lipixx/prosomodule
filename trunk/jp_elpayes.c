ac6d5bdab56347ed9f0fd0a7bfff8f9517a68af4
lipixx <lipixx@830a2072-bcc1-11dd-ad1f-f1f9622dbc98>
2008-12-14 16:55:14 +0000

    arreglats conflictes

--- trunk/jp_elpayes.c
+++ trunk/jp_elpayes.c
@@ -5,16 +5,22 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <stdlib.h>
-
+#include "include/mi_huerto.h"
 
 int main()
 {
   int fd, r, maj, min;
   char buffer[50];
+  struct pid_stats * stats;
+
   for(r=0;r<50;r++) buffer[r]='G';
   mknod("/dev/pages",254,0);
   fd=open("/dev/payes",O_RDONLY);
   r=read(fd,&buffer,50);
-  write(0,&buffer,50);
+  
+  stats = (struct pid_stats *) buffer;
+  printf("\na=%i\nb=%i\nc=%i\nd=%lld\n\n",stats->num_entrades,stats->num_sortides_ok,stats->num_sortides_error,stats->durada_total);
+
+
   exit(0);
 }
